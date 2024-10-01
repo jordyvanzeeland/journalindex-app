1a45ecd72c555138c7ccf1172cb588848b2b9c43
Jordy van Zeeland <jordyvanzeeland@gmail.com>
2024-10-01 08:54:50 +0200

    Setting up app

--- src/views/Categories.jsx
+++ src/views/Categories.jsx
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import { withRouter } from "../components/withRouter";
+import { useNavigate } from "react-router-dom";
 import { fetchApi } from "../Functions";
 import Sidebar from "../components/Sidebar";
 
 const Categories = (props) => {
+    const [categories, setCategories] = useState([]);
+    const navigate = useNavigate();
+
+    const getCategories = async () => {
+        const data = await fetchApi("GET", "/categories");
+        setCategories(data);
+    }
+
+    useEffect(() => {
+        getCategories();
+    }, [])
 
     return(
         <React.Fragment>
@@ -13,6 +25,25 @@ const Categories = (props) => {
             <div className="content">
                 <div className="container-fluid">
                     <h3>Categorieën</h3>
+
+                    <table className="table resonsive nowrap" width="100%">
+                        <thead>
+                            <tr>
+                                <th>Naam</th>
+                                <th>Beschrijving</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {categories.map((category, i) => {
+                                return(
+                                    <tr key={i}>
+                                        <td onClick={() => navigate(`/challenge/${category.id}`)}>{category.name}</td>
+                                        <td>{category.description}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </React.Fragment>
--- src/views/Journals.jsx
+++ src/views/Journals.jsx
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import { withRouter } from "../components/withRouter";
+import { useNavigate } from "react-router-dom";
 import { fetchApi } from "../Functions";
 import Sidebar from "../components/Sidebar";
 
 const Journals = (props) => {
+    const [journals, setJournals] = useState([]);
+    const navigate = useNavigate();
+
+    const getJournals = async () => {
+        const data = await fetchApi("GET", "/journals");
+        setJournals(data);
+    }
+
+    useEffect(() => {
+        getJournals();
+    }, [])
 
     return(
         <React.Fragment>
@@ -13,6 +25,27 @@ const Journals = (props) => {
             <div className="content">
                 <div className="container-fluid">
                     <h3>Journals</h3>
+
+                    <table className="table resonsive nowrap" width="100%">
+                        <thead>
+                            <tr>
+                                <th>Naam</th>
+                                <th>Beschrijving</th>
+                                <th>Datum</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {journals.map((journal, i) => {
+                                return(
+                                    <tr key={i}>
+                                        <td onClick={() => navigate(`/challenge/${journal.id}`)}>{journal.name}</td>
+                                        <td>{journal.description}</td>
+                                        <td>{journal.dateBegin} {journal.dateEnd}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </React.Fragment>
